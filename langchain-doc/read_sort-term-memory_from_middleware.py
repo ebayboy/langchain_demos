2ee92b25bf89d8ef35b345480e54a73e354755c3
fanpengfei1 <fanpengfei@jd.com>
2026-03-17 19:44:37 +0800

    fw

--- langchain-doc/read_sort-term-memory_from_middleware.py
+++ langchain-doc/read_sort-term-memory_from_middleware.py
@@ -21,18 +21,35 @@ def dynamic_system_prompt(request: ModelRequest) -> str:
     # 从短期记忆中读取用户名 user_name
     user_name = request.runtime.context["user_name"]
     system_prompt = f"You are a helpful assistant. Address the user as {user_name}."
+
+    print("system_prompt:", system_prompt)
     return system_prompt
 
 
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv, find_dotenv
+import os
+
+_ = load_dotenv(find_dotenv())
+
+api_key = os.getenv("OPENAI_API_KEY")
+base_url = os.getenv("OPENAI_API_BASE")
+model_name = os.getenv("OPENAI_MODEL_NAME")
+model = ChatOpenAI(api_key=api_key, base_url=base_url, model=model_name)
+
 agent = create_agent(
-    model="openai:gpt-5-nano",
+    model=model,
     tools=[get_weather],
     middleware=[dynamic_system_prompt],
     context_schema=CustomContext,
 )
 
 result = agent.invoke(
-    {"messages": [{"role": "user", "content": "What is the weather in SF?"}]},
+    {
+        "messages": [
+            {"role": "user", "content": "What is the weather in SF? " + "\\no_think"}
+        ]
+    },
     context=CustomContext(user_name="John Smith"),
 )
 for msg in result["messages"]:
