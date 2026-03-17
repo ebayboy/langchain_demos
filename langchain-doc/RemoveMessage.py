baaf6f8145e3c6e9410675de2baef88ece45fc7c
fanpengfei1 <fanpengfei@jd.com>
2026-03-17 19:20:02 +0800

    fw

---
+++ langchain-doc/RemoveMessage.py
@@ -0,0 +1,44 @@
+from langchain.messages import RemoveMessage
+from langchain.agents import create_agent, AgentState
+from langchain.agents.middleware import after_model
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.runtime import Runtime
+from langchain_core.runnables import RunnableConfig
+
+
+@after_model
+def delete_old_messages(state: AgentState, runtime: Runtime) -> dict | None:
+    """Remove old messages to keep conversation manageable."""
+    messages = state["messages"]
+    if len(messages) > 2:
+        # 删除指定消息 remove the earliest two messages
+        return {"messages": [RemoveMessage(id=m.id) for m in messages[:2]]}
+
+        # 删除所有消息 return {"messages": [RemoveMessage(id=REMOVE_ALL_MESSAGES)]}
+
+    return None
+
+
+agent = create_agent(
+    "openai:gpt-5-nano",
+    tools=[],
+    system_prompt="Please be concise and to the point.",
+    middleware=[delete_old_messages],
+    checkpointer=InMemorySaver(),
+)
+
+config: RunnableConfig = {"configurable": {"thread_id": "1"}}
+
+for event in agent.stream(
+    {"messages": [{"role": "user", "content": "hi! I'm bob"}]},
+    config,
+    stream_mode="values",
+):
+    print([(message.type, message.content) for message in event["messages"]])
+
+for event in agent.stream(
+    {"messages": [{"role": "user", "content": "what's my name?"}]},
+    config,
+    stream_mode="values",
+):
+    print([(message.type, message.content) for message in event["messages"]])
